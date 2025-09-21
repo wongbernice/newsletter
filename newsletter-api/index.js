5360ff40ff7d8ddd0f8697f7cfe29b4a30c07c54
wongbernice <byhwong@fierysnow.com>
2025-09-21 06:54:33 -0400

    added compile_newsletter api

--- newsletter-api/index.js
+++ newsletter-api/index.js
@@ -14,13 +14,6 @@ app.get("/", (req, res) => {
 });
 
 app.use('/create_group', createGroupRouter);
-/* Error handler middleware */
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  console.error(err.message, err.stack);
-  res.status(statusCode).json({ message: err.message });
-  return;
-});
 
 // call add_user_to_group.js
 const addUserToGroupRouter = require('./routes/addusertogroup');
@@ -33,6 +26,18 @@ app.use('/', getGroupsRouter);
 // call add_update.js
 const addUpdateRouter = require('./routes/addupdate');
 app.use('/', addUpdateRouter);
+
+// call compile_newsletter.js
+const compileNewsletterRouter = require('./routes/compilenewsletter');
+app.use('/compile_newsletter', compileNewsletterRouter);
+
+// error handler middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({ message: err.message });
+  return;
+});
 
 app.get("/db-test", (req, res) => {
     db.query("SELECT NOW() AS currentTime", (err, results) => {
